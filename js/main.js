f9c8b6521763062e42d3c76490ea6c0696e8960f
Jack Cambou <jcambou@gmail.com>
2018-05-21 11:43:26 -0700

    Added code for spider dropping on QWER and Space button presses

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 console.log("JS loaded")
 
 var $playerTwo = $("#player-two");
-var $gameboard = $("#gameboard")
+var $gameboard = $("#gameboard");
+var $spider = $(".spider")
 
 //Create Movement for player two based on key press
 
@@ -12,25 +13,68 @@ function keyDownHandler(e) {
    //Right Arrow
     if(e.keyCode == 39) {
         $playerTwo.animate({
-            left: "+=5px"
+            left: "+=15px"
         },10);
     }
     //Left Arrow
     else if(e.keyCode == 37) {
         $playerTwo.animate({
-            left: "-=5px"
+            left: "-=15px"
         },10);
     }
     //Down Arrow
-    if(e.keyCode == 40) {
+    else if(e.keyCode == 40) {
         $playerTwo.animate({
-            top: "+=5px"
+            top: "+=15px"
         },10);
     }
     //Up Arrow
     else if(e.keyCode == 38) {
         $playerTwo.animate({
-            top: "-=5px"
+            top: "-=15px"
         },10);
     }
+    //Spider drop on Q
+    else if(e.keyCode == 81){
+        $spider.animate({
+            top: "640px"
+        }, 3000
+        )
+    }
+    //Spider drop on W
+    else if(e.keyCode == 87){
+        $spider.animate({
+            top: "640px"
+        }, 3000
+        )
+    }
+    //Spider drop on E
+    else if(e.keyCode == 69){
+        $spider.animate({
+            top: "640px"
+        }, 3000
+        )
+    }
+    //Spider drop on R
+    else if(e.keyCode == 82){
+        $spider.animate({
+            top: "640px"
+        }, 3000
+        )
+    }
+    //Spider drop on Space
+    else if(e.keyCode == 32){
+        $spider.animate({
+            top: "640px"
+        }, 3000
+        )
+    }
 }
+
+function spiderThrow(){
+    //function for creating new spider
+    // newSpider.Animate(shoot spider to the bottom of the screen)
+}
+
+//Collision Detection
+
