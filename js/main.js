e8ed4506c7dea83f233c2b8dc2bd386ceb945372
Jack Cambou <jcambou@gmail.com>
2018-05-22 11:00:11 -0700

    Added spider counter which tracks spiders dodged as well as added in instructions for both players.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var $gameboard = $("#gameboard");
 var $spider = $(".spider")
 var $titleLine = $("#top-titleline");
 var spiderCounter = 0;
+var $spiderDodge = $("#counter")
+
 
 //Create Movement for player two based on key press
 
@@ -15,25 +17,25 @@ function keyDownHandler(e) {
     //Right Arrow
     if(e.keyCode == 39 && (($playerTwo.offset().left + $playerTwo.width()) < ($gameboard.offset().left + $gameboard.width()))) {
         $playerTwo.animate({
-            left: "+=15px"
+            left: "+=20px"
         },10);
     }
     //Left Arrow
     else if(e.keyCode == 37 && ($playerTwo.offset().left) > ($gameboard.offset().left)) {
         $playerTwo.animate({
-            left: "-=15px"
+            left: "-=20px"
         },10);
     }
     //Down Arrow
     else if(e.keyCode == 40 && (($playerTwo.offset().top + $playerTwo.height()) < ($gameboard.offset().top + $gameboard.height()))) {
         $playerTwo.animate({
-            top: "+=15px"
+            top: "+=20px"
         },10);
     }
     //Up Arrow
     else if(e.keyCode == 38 && ($playerTwo.offset().top > $gameboard.offset().top)) {
         $playerTwo.animate({
-            top: "-=15px"
+            top: "-=20px"
         },10);
     }
 }
@@ -43,27 +45,22 @@ function keyUpHandler(e){
     //Spider drop on Q
     if(e.keyCode == 81){
         new SpiderThrow(81);
-        spiderCounter += 1;
     }
     //Spider drop on W
     else if(e.keyCode == 87){
         new SpiderThrow(87)
-        spiderCounter += 1;
     }
     //Spider drop on E
     else if(e.keyCode == 69){
         new SpiderThrow(69);
-        spiderCounter += 1;
     }
     //Spider drop on R
     else if(e.keyCode == 82){
         new SpiderThrow(82)
-        spiderCounter += 1;
     }
     //Spider drop on Space
     else if(e.keyCode == 32){
         new SpiderThrow(32)
-        spiderCounter += 1;
     }
 }
 
@@ -81,13 +78,15 @@ function SpiderThrow(key){
     }   else if (key == 32){
         $newSpider.css({left: $gameboard.offset().left + $newSpider.width() + $newSpider.width() + $newSpider.width() + $newSpider.width()})
     }
-
+    
     //Interval for Spider movement
     var spiderMovement = setInterval(function(){
         $newSpider.css({top: "+=10px"});
         if(($newSpider.offset().top + $newSpider.height()) > $gameboard.height() + $titleLine.height()) {
             $newSpider.remove()
             clearInterval(spiderMovement)
+            spiderCounter += 1;
+            $spiderDodge.html(spiderCounter + " Spiders Dodged!")
         } 
         
         //Collision Detection for Spider to Corgi
@@ -98,6 +97,8 @@ function SpiderThrow(key){
             console.log("colision detected");
             $newSpider.remove()
             clearInterval(spiderMovement)
+            spiderCounter = 0
+            $spiderDodge.html("Bambi was DEVOURED!");
         }
     }, 50)
 }
