032e6440ad9ccca9d608e9e56145ac935227afa8
Jack Cambou <jcambou@gmail.com>
2018-05-22 12:06:12 -0700

    Working on improving movement with tracked keys array

--- js/main.js
+++ js/main.js
@@ -6,65 +6,72 @@ var $spider = $(".spider")
 var $titleLine = $("#top-titleline");
 var spiderCounter = 0;
 var $spiderDodge = $("#counter")
-
+var trackedKeys = undefined;
 
 //Create Movement for player two based on key press
 
-document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keydown", function(e){
+    trackedKeys.keys = (trackedKeys.keys || []);
+    trackedKeys.keys[e.keyCode] = true;
+});
+window.addEventListener('keyup', function (e) {
+    myGameArea.keys[e.keyCode] = false; 
+})
 
 // function For Player Two Movement
-function keyDownHandler(e) {
+ function playerTwoMovement() {
     //Right Arrow
-    if(e.keyCode == 39 && (($playerTwo.offset().left + $playerTwo.width()) < ($gameboard.offset().left + $gameboard.width()))) {
+    if(trackedKeys.keys && trackedKeys.keys[39] && (($playerTwo.offset().left + $playerTwo.width()) < ($gameboard.offset().left + $gameboard.width()))) {
         $playerTwo.animate({
             left: "+=20px"
         },10);
         $playerTwo.addClass('flip')
     }
     //Left Arrow
-    else if(e.keyCode == 37 && ($playerTwo.offset().left) > ($gameboard.offset().left)) {
+    if(trackedKeys.keys && trackedKeys.keys[37] && ($playerTwo.offset().left) > ($gameboard.offset().left)) {
         $playerTwo.animate({
             left: "-=20px"
         },10);
         $playerTwo.removeClass('flip')
     }
     //Down Arrow
-    else if(e.keyCode == 40 && (($playerTwo.offset().top + $playerTwo.height()) < ($gameboard.offset().top + $gameboard.height()))) {
+    if(trackedKeys.keys && trackedKeys.keys[40] && (($playerTwo.offset().top + $playerTwo.height()) < ($gameboard.offset().top + $gameboard.height()))) {
         $playerTwo.animate({
             top: "+=20px"
         },10);
     }
     //Up Arrow
-    else if(e.keyCode == 38 && ($playerTwo.offset().top > $gameboard.offset().top)) {
+    if(trackedKeys.keys && trackedKeys.keys[38] && ($playerTwo.offset().top > $gameboard.offset().top)) {
         $playerTwo.animate({
             top: "-=20px"
         },10);
     }
 }
+var checkPlayerTwoPosition = setInterval(playerTwoMovement, 10);
 
-document.addEventListener("keyup", keyUpHandler);
-function keyUpHandler(e){
-    //Spider drop on Q
-    if(e.keyCode == 81){
-        new SpiderThrow(81);
-    }
-    //Spider drop on W
-    else if(e.keyCode == 87){
-        new SpiderThrow(87)
-    }
-    //Spider drop on E
-    else if(e.keyCode == 69){
-        new SpiderThrow(69);
-    }
-    //Spider drop on R
-    else if(e.keyCode == 82){
-        new SpiderThrow(82)
-    }
-    //Spider drop on Space
-    else if(e.keyCode == 32){
-        new SpiderThrow(32)
-    }
-}
+// document.addEventListener("keyup", keyUpHandler);
+// function keyUpHandler(e){
+//     //Spider drop on Q
+//     if(e.keyCode == 81){
+//         new SpiderThrow(81);
+//     }
+//     //Spider drop on W
+//     else if(e.keyCode == 87){
+//         new SpiderThrow(87)
+//     }
+//     //Spider drop on E
+//     else if(e.keyCode == 69){
+//         new SpiderThrow(69);
+//     }
+//     //Spider drop on R
+//     else if(e.keyCode == 82){
+//         new SpiderThrow(82)
+//     }
+//     //Spider drop on Space
+//     else if(e.keyCode == 32){
+//         new SpiderThrow(32)
+//     }
+// }
 
 //Constructor function for making spider
 function SpiderThrow(key){
