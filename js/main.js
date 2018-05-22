63d8dddb27da975ca77aecd4b6127cd71a87def1
Jack Cambou <jcambou@gmail.com>
2018-05-22 12:38:30 -0700

    Improved Two Player Movement with Multi-Key Tracking

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ var $spider = $(".spider")
 var $titleLine = $("#top-titleline");
 var spiderCounter = 0;
 var $spiderDodge = $("#counter")
-var trackedKeys = undefined;
+var trackedKeys = new Array();
 
 //Create Movement for player two based on key press
 
 document.addEventListener("keydown", function(e){
     trackedKeys.keys = (trackedKeys.keys || []);
-    trackedKeys.keys[e.keyCode] = true;
+    console.log(trackedKeys)
+    trackedKeys.keys[e.keyCode] = (e.type == "keydown");
 });
 window.addEventListener('keyup', function (e) {
-    myGameArea.keys[e.keyCode] = false; 
+    trackedKeys.keys[e.keyCode] = (e.type == "keydown"); 
 })
 
 // function For Player Two Movement
@@ -23,55 +24,58 @@ window.addEventListener('keyup', function (e) {
     //Right Arrow
     if(trackedKeys.keys && trackedKeys.keys[39] && (($playerTwo.offset().left + $playerTwo.width()) < ($gameboard.offset().left + $gameboard.width()))) {
         $playerTwo.animate({
-            left: "+=20px"
+            left: "+=18px"
         },10);
         $playerTwo.addClass('flip')
     }
     //Left Arrow
     if(trackedKeys.keys && trackedKeys.keys[37] && ($playerTwo.offset().left) > ($gameboard.offset().left)) {
         $playerTwo.animate({
-            left: "-=20px"
+            left: "-=18px"
         },10);
         $playerTwo.removeClass('flip')
     }
     //Down Arrow
     if(trackedKeys.keys && trackedKeys.keys[40] && (($playerTwo.offset().top + $playerTwo.height()) < ($gameboard.offset().top + $gameboard.height()))) {
         $playerTwo.animate({
-            top: "+=20px"
+            top: "+=18px"
         },10);
     }
     //Up Arrow
     if(trackedKeys.keys && trackedKeys.keys[38] && ($playerTwo.offset().top > $gameboard.offset().top)) {
         $playerTwo.animate({
-            top: "-=20px"
+            top: "-=18px"
         },10);
     }
 }
-var checkPlayerTwoPosition = setInterval(playerTwoMovement, 10);
+// Set Interval to constantly be checking player two position
+var checkPlayerTwoPosition = setInterval(playerTwoMovement, 75);
 
-// document.addEventListener("keyup", keyUpHandler);
-// function keyUpHandler(e){
-//     //Spider drop on Q
-//     if(e.keyCode == 81){
-//         new SpiderThrow(81);
-//     }
-//     //Spider drop on W
-//     else if(e.keyCode == 87){
-//         new SpiderThrow(87)
-//     }
-//     //Spider drop on E
-//     else if(e.keyCode == 69){
-//         new SpiderThrow(69);
-//     }
-//     //Spider drop on R
-//     else if(e.keyCode == 82){
-//         new SpiderThrow(82)
-//     }
-//     //Spider drop on Space
-//     else if(e.keyCode == 32){
-//         new SpiderThrow(32)
-//     }
-// }
+function playerOneMovement(){
+    //Spider drop on Q
+    if(trackedKeys.keys && trackedKeys.keys[81]){
+        new SpiderThrow(81);
+    }
+    //Spider drop on W
+    else if(trackedKeys.keys && trackedKeys.keys[87]){
+        new SpiderThrow(87)
+    }
+    //Spider drop on E
+    else if(trackedKeys.keys && trackedKeys.keys[69]){
+        new SpiderThrow(69);
+    }
+    //Spider drop on R
+    else if(trackedKeys.keys && trackedKeys.keys[82]){
+        new SpiderThrow(82)
+    }
+    //Spider drop on Space
+    else if(trackedKeys.keys && trackedKeys.keys[32]){
+        new SpiderThrow(32)
+    }
+}
+
+//Interval Checking Player one holding things down
+var checkPlayerOnePosition = setInterval(playerOneMovement, 950);
 
 //Constructor function for making spider
 function SpiderThrow(key){
