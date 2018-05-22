518713a59d86b321d0e1a18f359157f881693564
Jack Cambou <jcambou@gmail.com>
2018-05-22 09:01:09 -0700

    Coded logic to allow spider drops to be designated to specfic keys and by passing key code as argument through keycode in SpiderThrow constructor function

--- js/main.js
+++ js/main.js
@@ -41,37 +41,45 @@ document.addEventListener("keyup", keyUpHandler);
 function keyUpHandler(e){
     //Spider drop on Q
     if(e.keyCode == 81){
-        new SpiderThrow();
+        new SpiderThrow(81);
         spiderCounter += 1;
     }
     //Spider drop on W
     else if(e.keyCode == 87){
-        new SpiderThrow()
+        new SpiderThrow(87)
         spiderCounter += 1;
     }
     //Spider drop on E
     else if(e.keyCode == 69){
-        new SpiderThrow();
+        new SpiderThrow(69);
         spiderCounter += 1;
     }
     //Spider drop on R
     else if(e.keyCode == 82){
-        new SpiderThrow()
+        new SpiderThrow(82)
         spiderCounter += 1;
     }
     //Spider drop on Space
     else if(e.keyCode == 32){
-        new SpiderThrow()
+        new SpiderThrow(32)
         spiderCounter += 1;
     }
 }
 
 //Constructor function for making spider
-function SpiderThrow(){
+function SpiderThrow(key){
     //Linking to spider Image and appending to game board
     var $newSpider = $("<img class='spider' src='images/spider.png'>");
     $gameboard.append($newSpider);
-
+    if(key == 87){
+        $newSpider.css({left: $gameboard.offset().left + $newSpider.width()})
+    } else if (key == 69){
+        $newSpider.css({left: $gameboard.offset().left + $newSpider.width() + $newSpider.width()})
+    } else if (key == 82){
+        $newSpider.css({left: $gameboard.offset().left + $newSpider.width()+ $newSpider.width() + $newSpider.width()})
+    }   else if (key == 32){
+        $newSpider.css({left: $gameboard.offset().left + $newSpider.width() + $newSpider.width() + $newSpider.width() + $newSpider.width()})
+    }
 
     //Interval for Spider movement
     var spiderMovement = setInterval(function(){
@@ -86,8 +94,9 @@ function SpiderThrow(){
         $newSpider.offset().left + $newSpider.width() > $playerTwo.offset().left &&
         $newSpider.offset().top < ($playerTwo.offset().top + $playerTwo.height()) &&
         $newSpider.offset().top + $newSpider.height() > $playerTwo.offset().top){
-            console.log("collision detected");
+            console.log("colision detected");
             $newSpider.remove()
+            clearInterval(spiderMovement)
         }
     }, 50)
 }
